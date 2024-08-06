5a3ce1aa3b94e8b21a628e1675377ae61cab2c11
Zigao Wang <zigao.wang24@gmail.com>
2024-08-06 08:29:33 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///students.db'
-    SQLALCHEMY_BINDS = {
-        'conversations': 'sqlite:///conversations.db'
-    }
-    SESSION_TYPE = 'filesystem'
